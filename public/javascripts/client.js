99fbc2f481f80c5e9de39bd51969571447ed0776
duyicong <574834341@qq.com>
2017-08-31 22:29:15 -0700

    Initial commit

---
+++ public/javascripts/client.js
@@ -0,0 +1,98 @@
+$(function () {
+    var socket = io();
+    var username;
+    var password;
+
+    /*
+    * when click on log_in,verify username and password by sending data to backend
+    * if succeed, then reload all the history messages
+    * else give some hints
+    * */
+    $("#log_in").click(function () {
+        username=$(".EnterUsrName").val();
+        password=$("#EnterPsw").val();
+        var data = {"username":username,"password":password};
+        $.ajax({
+            url: '/index',
+            type: 'post',
+            data: data,
+            success: function (data, status) {
+                if (status=='success') {
+                    $('.LoginPage').hide();
+                    $('.MainPage').show();
+                    socket.emit("get history messages",function () {});
+                }
+            },
+            error: function (data, status) {
+                if (status=='error') {
+                    alert("wrong username or password");
+                }
+            }
+        });
+    })
+    /*
+        * when click on register, send user name and password to backend
+        * if succeed, then reload all the history messages
+        * else give some hints like duplicate username
+        * */
+    $("#register").click(function () {
+        username=$(".EnterUsrName").val();
+        password=$("#EnterPsw").val();
+        var data = {"username":username,"password":password};
+        $.ajax({
+            url: '/index_register',
+            type: 'post',
+            data: data,
+            success: function (data, status) {
+                if (status=='success') {
+                    alert("Register successfully! Please remember your password to log in next time.");
+                    $('.LoginPage').hide();
+                    $('.MainPage').show();
+                    socket.emit("get history messages",function () {});
+                }
+            },
+            error: function (data, status) {
+                if (status=='error') {
+                    alert("User name has been used. Please try another one");
+                }
+            }
+        });
+
+    })
+    /*
+    * When click on leave, back to the main page
+    * Leave all the spaces blank
+    * */
+    $("#leave").click(function () {
+        $('.MainPage').hide();
+        $('.LoginPage').show();
+        $(".EnterUsrName").val('');
+        $("#enterpsw").val('');
+
+    })
+    /*
+    * when click on submit, get the message that submitted
+    * sends to the back end using socket
+    * it will be recorded in database and show to all users
+    * */
+
+    $(".submitMessage").click(function () {
+        var time=new Date();
+        socket.emit("send message",$('.inputMessage').val(),username,time.toLocaleString());
+        // make the input area blank after submit
+        $(".inputMessage").val('');
+
+    })
+
+    socket.on('show message',function(msg,username,time){
+        var msg_formatting='<span class = MsgContent><br>'+msg+'</span>';
+        var usr_formatting='<span class = MsgUsr>'+username+'</span>';
+        var time_formatting='<span class = MsgTime>'+time+'</span>';
+        $('.ShowMsg').append($('<li>'+usr_formatting+time_formatting+msg_formatting+'</li>'));
+        //make sure always show the latest message, scroll the panal down to the end
+        $('.ShowMsg').scrollTop($('.ShowMsg')[0].scrollHeight);
+
+    })
+
+
+});
---
+++ routes/index.js
@@ -0,0 +1,117 @@
+var socketio = {};
+var express = require('express');
+var router = express.Router();
+var socket_io = require('socket.io');
+var mysql = require('mysql');
+
+
+var db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'newpass',
+    database: 'msg_info',
+});
+
+
+router.post('/index', function (req,res,next){
+  var username=req.body.username;
+  var password=req.body.password;
+  db.query("SELECT * FROM msg_info.user_info WHERE password= '"+password+"' AND username='"+username+"'",function(err,rows){
+        if(err) throw err;
+        if (rows.length>0) {res.send('1');}
+        else
+            {
+                res.send(404);
+            }
+
+    });
+
+})
+
+router.post('/index', function (req,res,next){
+    var username=req.body.username;
+    var password=req.body.password;
+    db.query("SELECT * FROM msg_info.user_info WHERE password= '"+password+"' AND username='"+username+"'",function(err,rows){
+        if(err) throw err;
+        if (rows.length>=0) {res.send('1');}
+        else
+        {
+            res.send(404);
+        }
+
+    });
+
+})
+router.post('/index_register', function (req,res,next){
+    var username=req.body.username;
+    var password=req.body.password;
+    db.query("SELECT * FROM msg_info.user_info WHERE username='"+username+"'",function(err,rows){
+        if(err) throw err;
+        if (rows.length==0) {
+          res.send('1');
+          var insert_usr={username:username,password:password};
+          db.query('INSERT INTO user_info SET ?',insert_usr, function(err,res){
+                if(err) throw err;
+            });
+        }
+        else
+        {
+            res.send(500);
+        }
+
+    });
+
+})
+
+
+socketio.getSocketio = function(server){
+    var io = socket_io.listen(server);
+    io.on('connection', function(socket){
+
+        socket.on('get history messages',function () {
+            db.query('SELECT * FROM msg_info.messages_info', function (err, rows) {
+                if (err) throw err;
+
+                for (var i = 0; i < rows.length; i++) {
+                    var username = rows[i].username;
+                    var time = rows[i].msgtime;
+                    var msg = rows[i].msg;
+                    socket.emit('show message', msg, username, time);
+                }
+            })
+        })
+
+
+        socket.on('send message',function(msg,usr_name,time) {
+            var insert_info={username:usr_name,msg:msg,msgtime:time};
+
+            db.query('INSERT INTO messages_info SET ?',insert_info, function(err,res){
+                if(err) throw err;
+                //console.log(message);
+            });
+            socket.broadcast.emit('show message',msg,usr_name,time);
+            socket.emit('show message',msg,usr_name,time);
+
+        });
+
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
+
+
+
+    });
+
+
+};
+
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+
+
+var data={"socketio":socketio,"router":router}
+module.exports = data;
+
