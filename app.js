127262dcedb8d47eb81db2949da46cbf828d614a
DuYicong515 <574834341@qq.com>
2017-08-31 22:43:46 -0700

    add comment to the place I have changed

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//change the return file of index.js
 var index = require('./routes/index').router;
 var users = require('./routes/users');
 
